b13c8cb8300cce4daefe414ee7be1e855dcc2157
tongliyan <tongliyan@milibaitiao.com>
2018-03-23 18:12:23 +0800

    Add new file.

---
+++ for-algorithm/src/main/java/com/alexstudy/hackrank/ElectronicsShopSolution.java
@@ -0,0 +1,11 @@
+package com.alexstudy.hackrank;
+
+/**
+ * @author AlexTong
+ * @ClassName ElectronicsShopSolution
+ * @Description TODO()
+ * @date 2018/3/23 18:11:35
+ */
+public class ElectronicsShopSolution {
+    
+}
